db5a0c23e7ecb645681f554f1badaa32491ee97a
temka5228 <artemlazarev2015@gmail.com>
2022-07-21 15:39:09 +0500

    got rid of the pyradiomics

--- main.py
+++ main.py
@@ -53,54 +53,102 @@ def plot(data):
     pyplot.show()
 
 
-def get_mean(img_ord: firstorder.RadiomicsFirstOrder):
-    count = float(img_ord.getMeanFeatureValue())
-    return count
+def get_masked_image(img_array: np.ndarray, mask_array: np.ndarray):
+    return np.multiply(img_array, mask_array)
 
 
-def get_sd(img_ord: firstorder.RadiomicsFirstOrder):
-    count = float(img_ord.getStandardDeviationFeatureValue())
-    return count
+def get_coordinates(mask: np.ndarray):
+    coord_array = []
+    for slice in range(len(mask)):
+        for coloumn in range(len(mask[0])):
+            for a in range(len(mask[0][0])):
+                if mask[slice][coloumn][a] != 0:
+                    coord_array.append([slice, coloumn, a])
+    return np.array(coord_array)
 
 
-def get_median(img_ord: firstorder.RadiomicsFirstOrder):
-    count = float(img_ord.getMedianFeatureValue())
-    return count
+def get_mean(img_array: np.ndarray, coord: np.ndarray):
+    summary: int = 0
+    for i in range(len(coord)):
+        slice = coord[i][0]
+        coloumn = coord[i][1]
+        row = coord[i][2]
+        summary += img_array[slice][coloumn][row]
+    return summary / len(coord)
 
 
-def get_diam_x(img_sh: shape.RadiomicsShape):
-    count = float(img_sh.getMaximum2DDiameterColumnFeatureValue())
-    return count
+def get_sd(img_array: np.ndarray, coord: np.ndarray, mean: float):
+    summary: int = 0
+    for i in range(len(coord)):
+        slice = coord[i][0]
+        coloumn = coord[i][1]
+        row = coord[i][2]
+        summary += np.square(img_array[slice][coloumn][row] - mean)
+    return np.sqrt(summary / len(coord))
 
 
-def get_diam_y(img_sh: shape.RadiomicsShape):
-    count = float(img_sh.getMaximum2DDiameterRowFeatureValue())
-    return count
+def get_median(img_array: np.ndarray, coord: np.ndarray):
+    summary: list = []
+    for i in range(len(coord)):
+        slice = coord[i][0]
+        coloumn = coord[i][1]
+        row = coord[i][2]
+        summary.append(img_array[slice][coloumn][row])
+    ar = np.array(summary)
+    ar = np.sort(ar)
+    return np.median(ar)
 
 
-def get_diam_z(img_sh: shape.RadiomicsShape):
-    count = float(img_sh.getMaximum2DDiameterSliceFeatureValue())
-    return count
+def get_diams(mask: np.ndarray, ps: np.ndarray):
+    min_z = len(mask)
+    max_z = 0
+    x, y, z = 0, 0, 0
+    for slice in range(len(mask)):
+        min_i, min_j = len(mask[0]), len(mask[0][0])
+        max_i, max_j = 0, 0
+        for coloumn in range(len(mask[0])):
+            for a in range(len(mask[0][0])):
+                if mask[slice][coloumn][a] != 0:
+                    if a < min_i:
+                        min_i = a
+                    if coloumn < min_j:
+                        min_j = coloumn
+                    if a > max_i:
+                        max_i = a
+                    if coloumn > max_j:
+                        max_j = coloumn
+                    if slice < min_z:
+                        min_z = slice
+                    if slice > max_z:
+                        max_z = slice
+        x_pr = max_i - min_i
+        y_pr = max_j - min_j
+
+        if x_pr > x:
+            x = x_pr
+        if y_pr > y:
+            y = y_pr
+    x = x * ps[0]
+    y = y * ps[1]
+    z = (max_z - min_z) * ps[2]
+    return x, y, z
 
 
-def get_major(img_sh: shape.RadiomicsShape):
-    count = float(img_sh.getMajorAxisLengthFeatureValue())
-    return count
+def get_voxel_volume(mask_coord: np.ndarray, sp: np.ndarray):
+    return len(mask_coord) * sp[0] * sp[1] * sp[2]
 
 
-def get_minor(img_sh: shape.RadiomicsShape):
-    count = float(img_sh.getMinorAxisLengthFeatureValue())
-    return count
-
-
-def get_mesh_volume(img_sh: shape.RadiomicsShape):
-    count = float(img_sh.getMeshVolumeFeatureValue())
-    return count
-
-
-def get_voxel_volume(img_sh: shape.RadiomicsShape):
-    count = float(img_sh.getVoxelVolumeFeatureValue())
-    return count
+def get_major_minor(mask_array: np.ndarray, ps: np.ndarray):
+    labelledVoxelCoordinates = np.where(mask_array != 0)
+    Np = len(labelledVoxelCoordinates[0])
+    coordinates = np.array(labelledVoxelCoordinates, dtype='int').transpose((1, 0))
+    physicalCoordinates = coordinates * ps[None, :]
+    physicalCoordinates -= np.mean(physicalCoordinates, axis=0)  # Centered at 0
+    physicalCoordinates /= np.sqrt(Np)
+    covariance = np.dot(physicalCoordinates.T.copy(), physicalCoordinates)
+    eigenValues = np.linalg.eigvals(covariance)
+    eigenValues.sort()
+    return np.sqrt(eigenValues[2]) * 4, np.sqrt(eigenValues[1]) * 4
 
 
 if __name__ == "__main__":
@@ -109,26 +157,26 @@ if __name__ == "__main__":
     mask_nrrd = open_nrrd(mask_dir, mask_name)
     image_nrrd = open_nrrd(nrrd_dir, nrrd__image_name)
 
+    spacing = np.array(image_nrrd.GetSpacing())
+
     mask_nrrd_array = get_array(mask_nrrd)
     image_nrrd_array = get_array(image_nrrd)
 
     img_orders = first_order(image_nrrd, mask_nrrd)
     img_shape = img_to_shape(image_nrrd, mask_nrrd)
 
-    mean = get_mean(img_orders)
-    sd = get_sd(img_orders)
-    median = get_median(img_orders)
-    x = get_diam_x(img_shape)
-    y = get_diam_y(img_shape)
-    z = get_diam_z(img_shape)
-    major = get_major(img_shape)
-    minor = get_minor(img_shape)
-    mesh_volume = get_mesh_volume(img_shape)
-    voxel_volume = get_voxel_volume(img_shape)
+    masked_image = get_masked_image(image_nrrd_array, mask_nrrd_array)
+
+    mask_coord = get_coordinates(mask_nrrd_array)
+
+    mean = get_mean(image_nrrd_array, mask_coord)
+    sd = get_sd(image_nrrd_array, mask_coord, mean)
+    median = get_median(image_nrrd_array, mask_coord)
+    x, y, z = get_diams(mask_nrrd_array, spacing)
+    minor, major = get_major_minor(mask_nrrd_array, spacing)
+    voxel_volume = get_voxel_volume(mask_coord, spacing)
 
     print(f' mean = {mean}\n sd = {sd}\n median = {median}\n x = {x}\n y = {y}\n z = {z}\n major = {major}\n '
-          f'minor = {minor}\n mesh volume = {mesh_volume}\n voxel volume = {voxel_volume}')
+          f'minor = {minor}\n major = {major}\n voxel volume = {voxel_volume}')
 
-    plot(image_nrrd_array[10])
-
-
+    plot(masked_image[10])
